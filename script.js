470b049de4a1c22ff186d59328465741f295d6af
Glyph8 <newdy0808@gmail.com>
2025-12-31 11:03:07 +0900

    feat: 예약 견적서 초안 작성

---
+++ script.js
@@ -0,0 +1,9 @@
+const bornYear = 2000;
+let currentAge = 26;
+
+console.log(`Born Year: ${bornYear}`);
+console.log(`Current Age: ${currentAge}`);
+
+const favoriteFood = "Sushi";
+let feelingToday = 6;
+const today = "2025-12-31";
