a09c96805a49dd0bacb7a4b61c6d20be9a27c1e1
glyph8 <newdy0808@gmail.com>
2026-01-04 02:19:55 +0900

    feat: 폰트 파일 및 적용 추가, 예약 정보 페이지 작성

--- script.js
+++
@@ -1,9 +0,0 @@
-const bornYear = 2000;
-let currentAge = 26;
-
-console.log(`Born Year: ${bornYear}`);
-console.log(`Current Age: ${currentAge}`);
-
-const favoriteFood = "Sushi";
-let feelingToday = 6;
-const today = "2025-12-31";
